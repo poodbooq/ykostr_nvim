fbe7750d0a9640d582e102e836515cf0923e1689
Yevhen Kostryka <55651586+poodbooq@users.noreply.github.com>
2024-11-06 19:46:55 +0200

    Update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
     },
     setup = {
         gopls = function(_, opts)
+            local lsp = require 'lspconfig'
             -- workaround for gopls not supporting semanticTokensProvider
             -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
             lsp.on_attach(function(client, _)
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,5 +1,32 @@
 return {
     {
+        'echasnovski/mini.ai',
+        opts = function()
+            local ai = require 'mini.ai'
+            return {
+                n_lines = 500,
+                custom_textobjects = {
+                    o = ai.gen_spec.treesitter { -- code block
+                        a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+                        i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+                    },
+                    f = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' }, -- function
+                    c = ai.gen_spec.treesitter { a = '@class.outer', i = '@class.inner' }, -- class
+                    t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' }, -- tags
+                    d = { '%f[%d]%d+' }, -- digits
+                    e = { -- Word with case
+                        { '%u[%l%d]+%f[^%l%d]', '%f[%S][%l%d]+%f[^%l%d]', '%f[%P][%l%d]+%f[^%l%d]', '^[%l%d]+%f[^%l%d]' },
+                        '^().*()$',
+                    },
+                    -- i = ai.gen_spec.indent(), -- indent
+                    -- g = ai.gen_spec.buffer(), -- buffer
+                    u = ai.gen_spec.function_call(), -- u for "Usage"
+                    U = ai.gen_spec.function_call { name_pattern = '[%w_]' }, -- without dot in function name
+                },
+            }
+        end,
+    },
+    {
         'echasnovski/mini.nvim',
         version = false,
     },
